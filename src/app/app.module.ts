4613c9fea43525950b8b4121a8d60bb8d430df29
pavithra <pavithra@citrisys.com>
2023-01-26 14:25:19 +0530

    contact update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,15 +15,15 @@ import { AppComponent } from './app.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { FormsModule } from '@angular/forms';
 
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { ContactComponent } from './contact/contact.component';
 
 
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-  name: string = "";
-  email: string = "";
-  message: string = "";
-  constructor(){}
-  submitForm() {
-    console.log(this.name, this.email, this.message);
-    // send the form data to an API endpoint or email address
+export class ContactComponent  {
+ 
+  inputValue: string;
+  inputValue1: string;
+  inputValue2: string;
+  inputValue3: string;
+  constructor() {
+    this.inputValue = "";
+    this.inputValue1 = "";
+    this.inputValue2 = "";
+    this.inputValue3 = "";
+   
   }
-  buttonAction: any; 
+  
+
+  myimage:string="assets/pro-image/l3.png"
+  myimage1:string="assets/pro-image/p3.png"
+  myimage2:string="assets/pro-image/m3.png"
+
+  address = {
+    office:'Yolo Life',
+    location:'(A div. of Thanga Mayil Agro Pvt. Ltd.),',
+    street: 'Regd. Office: 8 Second Avenue,',
+    city: 'Sundar Nagar, Ekkattuthangal',
+    address: 'Chennai 600032',
+    phoneNo:'+91.72009 67561',
+    Email:'customercare@yololife.in'
+
+  };
+  buttonAction() {
+  }
 }
