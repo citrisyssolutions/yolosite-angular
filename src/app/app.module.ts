c348326e27564b720ccd8c75e97c73b24c493f42
pavithra <pavithra@citrisys.com>
2023-01-28 16:24:53 +0530

    contact update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,8 +29,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import 'jquery';
-import 'popper.js';
+
 
 @NgModule({
   declarations: [
